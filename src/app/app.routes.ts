1b8a65d5b91e7068bd3c8519bfe2b77937dad272
Ibrahim <u20191E650@upc.edu.pe>
2024-05-16 16:23:52 -0500

    se completo el navbar para la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 import { NavBarComponent } from './components/navbar/navbar.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 export const routes: Routes = [
     { path: 'landing-page', component: LandingPageComponent, title: 'ReservaMentor'},
     { path: 'navbar', component: NavBarComponent},
-    { path: '**', redirectTo: '/busquedaMentores', pathMatch: 'full'}
+    { path: 'busqueda', component: BusquedaComponent},
+    { path: '**', redirectTo: '/busqueda', pathMatch: 'full'}
 ];
---
+++ src/app/components/busqueda/busqueda.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-busqueda',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent {
+
+}
