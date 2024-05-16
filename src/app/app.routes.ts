42a783dbe23f355f378ee84fe27aa43932399260
Ibrahim <u20191E650@upc.edu.pe>
2024-05-16 10:19:51 -0500

    se agrego la primera version del navbar para la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 
+import { NavBarComponent } from './components/navbar/navbar.component';
+
 export const routes: Routes = [
+    { path: 'navbar', component: NavBarComponent},
+    { path: '**', redirectTo: '/navbar', pathMatch: 'full'}
 ];
