21a7eef6afe3ad545ef5be7fadc8164bd59ee58b
Ibrahim <u20191E650@upc.edu.pe>
2024-05-16 20:56:52 -0500

    se agrego la visibilidad de los mentores desde el json y se agrego el disenio de abajo del navbar

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../navbar/navbar.component';
-
+import { MentorService } from '../../services/mentor.service';
+import { Mentor } from '../../models/mentor';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 @Component({
   selector: 'app-busqueda',
   standalone: true,
-  imports: [NavBarComponent],
+  imports:  [NavBarComponent, NgIf, NgForOf],
+  providers: [HttpClientModule, CommonModule, BrowserModule],
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent {
+export class BusquedaComponent implements OnInit {
+  mentores: Mentor[] = [];
 
+
+  constructor(private mentorService: MentorService, private http: HttpClient) {
+    mentorService = new MentorService(http)
+    this.obtenerMentores();
+  }
+
+
+  ngOnInit(): void {
+  }
+
+
+  obtenerMentores(): void {
+    this.mentorService.getData().subscribe(
+      (data: Mentor[]) => {
+        this.mentores = data;
+      },
+      (error) => {
+        console.error('Error al obtener mentores:', error);
+        // Manejar el error apropiadamente
+      }
+    );
+  }
 }
