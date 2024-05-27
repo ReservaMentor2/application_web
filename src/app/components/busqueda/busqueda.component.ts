7fe17fdd19b51e90cb79a7288edc03be5bc8b651
Ibrahim <u20191E650@upc.edu.pe>
2024-05-26 19:19:29 -0500

    se agrego mas datos al json, se mejoro la funcionalidad de busqueda para buscar mejor xd,

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../navbar/navbar.component';
 import { MentorService } from '../../services/mentor.service';
-import { Mentor } from '../../models/mentor';
+import { HorarioDisponible, Mentor } from '../../models/mentor';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,10 +11,10 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-busqueda',
   standalone: true,
-  imports: [NavBarComponent,FooterComponent, NgIf, NgForOf, FormsModule],
+  imports: [NavBarComponent, FooterComponent, NgIf, NgForOf, FormsModule],
   providers: [HttpClientModule, CommonModule, BrowserModule],
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
 })
 export class BusquedaComponent implements OnInit {
   mentores: Mentor[] = [];
@@ -23,11 +23,13 @@ export class BusquedaComponent implements OnInit {
   selectedCategories: string[] = [];
   sortOption: string = '';
   searchTopic: string = '';
-  searchDate: string = '';
+  startDate: string = '';
+  endDate: string = '';
+  startTime: string = '';
+  endTime: string = '';
   selectedSessionTypes: string[] = [];
   selectedRatings: number[] = [];
   maxPrice: number = 0;
-  selectedHorarios: string[] = [];
 
   constructor(private mentorService: MentorService, private http: HttpClient) {
     this.mentorService = new MentorService(http);
@@ -41,33 +43,95 @@ export class BusquedaComponent implements OnInit {
       (data: Mentor[]) => {
         this.mentores = data;
         this.filteredMentores = data;
-        this.categories = Array.from(new Set(data.flatMap(mentor => mentor.categorias)));
+        this.categories = Array.from(
+          new Set(data.flatMap((mentor) => mentor.categorias))
+        );
       },
       (error) => {
         console.error('Error al obtener mentores:', error);
       }
     );
   }
+  onSearch(): void {
+    this.filterMentores();
+  }
 
   filterMentores(): void {
-    this.filteredMentores = this.mentores.filter(mentor => {
-      const matchesCategory = this.selectedCategories.length === 0 || this.selectedCategories.some(category => mentor.categorias.includes(category));
-      const matchesTopic = !this.searchTopic || mentor.categorias.includes(this.searchTopic);
-      const matchesDate = !this.searchDate || mentor.horariosDisponibles.includes(this.getDayPeriod(this.searchDate));
-      const matchesSessionType = this.selectedSessionTypes.length === 0 || this.selectedSessionTypes.some(type => mentor.tiposSesiones.includes(type));
-      const matchesRating = this.selectedRatings.length === 0 || this.selectedRatings.includes(Math.round(mentor.calificacion));
-      const matchesPrice = this.maxPrice === 0 || mentor.tarifaPorHora <= this.maxPrice;
-      const matchesHorario = this.selectedHorarios.length === 0 || this.selectedHorarios.some(horario => mentor.horariosDisponibles.includes(horario));
+    this.filteredMentores = this.mentores.filter((mentor) => {
+      const matchesCategory =
+        this.selectedCategories.length === 0 ||
+        this.selectedCategories.some((category) =>
+          mentor.categorias.includes(category)
+        );
+      const matchesTopic =
+        !this.searchTopic || mentor.categorias.includes(this.searchTopic);
+      const matchesDate = this.isWithinSelectedDateTimeRange(
+        mentor.horariosDisponibles
+      );
+      const matchesSessionType =
+        this.selectedSessionTypes.length === 0 ||
+        this.selectedSessionTypes.some((type) =>
+          mentor.tiposSesiones.includes(type)
+        );
+      const matchesRating =
+        this.selectedRatings.length === 0 ||
+        this.selectedRatings.includes(Math.round(mentor.calificacion));
+      const matchesPrice =
+        this.maxPrice === 0 || mentor.tarifaPorHora <= this.maxPrice;
 
-      return matchesCategory && matchesTopic && matchesDate && matchesSessionType && matchesRating && matchesPrice && matchesHorario;
+      return (
+        matchesCategory &&
+        matchesTopic &&
+        matchesDate &&
+        matchesSessionType &&
+        matchesRating &&
+        matchesPrice
+      );
     });
 
     this.sortMentores();
   }
 
+  isWithinSelectedDateTimeRange(
+    horariosDisponibles: HorarioDisponible[]
+  ): boolean {
+    if (!this.startDate || !this.endDate || !this.startTime || !this.endTime) {
+      return true;
+    }
+
+    const startDateTime = new Date(`${this.startDate}T${this.startTime}`);
+    const endDateTime = new Date(`${this.endDate}T${this.endTime}`);
+
+    return horariosDisponibles.some((horario) => {
+      const horarioStart = new Date(`${horario.fecha}T${horario.inicio}`);
+      const horarioEnd = new Date(`${horario.fecha}T${horario.fin}`);
+      return (
+        (horarioStart >= startDateTime && horarioStart <= endDateTime) ||
+        (horarioEnd >= startDateTime && horarioEnd <= endDateTime) ||
+        (horarioStart <= startDateTime && horarioEnd >= endDateTime)
+      );
+    });
+  }
+
+  isWithinSelectedDateRange(horariosDisponibles: HorarioDisponible[]): boolean {
+    if (!this.startDate || !this.endDate) {
+      return true;
+    }
+
+    const start = new Date(this.startDate);
+    const end = new Date(this.endDate);
+
+    return horariosDisponibles.some((horario) => {
+      const date = new Date(horario.fecha);
+      return date >= start && date <= end;
+    });
+  }
+
   toggleCategory(category: string): void {
     if (this.selectedCategories.includes(category)) {
-      this.selectedCategories = this.selectedCategories.filter(cat => cat !== category);
+      this.selectedCategories = this.selectedCategories.filter(
+        (cat) => cat !== category
+      );
     } else {
       this.selectedCategories.push(category);
     }
@@ -77,13 +141,14 @@ export class BusquedaComponent implements OnInit {
   onMaxPriceChange(event: any): void {
     // Si el campo de entrada está vacío, establece maxPrice en 0, de lo contrario, usa el valor ingresado
     this.maxPrice = event.target.value ? parseFloat(event.target.value) : 0;
-    this.filterMentores(); // Asegúrate de que el filtro se actualice con el nuevo valor de maxPrice
+    this.filterMentores();
   }
-
 
   toggleSessionType(sessionType: string): void {
     if (this.selectedSessionTypes.includes(sessionType)) {
-      this.selectedSessionTypes = this.selectedSessionTypes.filter(st => st !== sessionType);
+      this.selectedSessionTypes = this.selectedSessionTypes.filter(
+        (st) => st !== sessionType
+      );
     } else {
       this.selectedSessionTypes.push(sessionType);
     }
@@ -92,18 +157,9 @@ export class BusquedaComponent implements OnInit {
 
   toggleRating(rating: number): void {
     if (this.selectedRatings.includes(rating)) {
-      this.selectedRatings = this.selectedRatings.filter(r => r !== rating);
+      this.selectedRatings = this.selectedRatings.filter((r) => r !== rating);
     } else {
       this.selectedRatings.push(rating);
-    }
-    this.filterMentores();
-  }
-
-  toggleHorario(horario: string): void {
-    if (this.selectedHorarios.includes(horario)) {
-      this.selectedHorarios = this.selectedHorarios.filter(h => h !== horario);
-    } else {
-      this.selectedHorarios.push(horario);
     }
     this.filterMentores();
   }
@@ -117,10 +173,14 @@ export class BusquedaComponent implements OnInit {
         this.filteredMentores.sort((a, b) => b.calificacion - a.calificacion);
         break;
       case 'students-asc':
-        this.filteredMentores.sort((a, b) => a.estudiantesAyudados - b.estudiantesAyudados);
+        this.filteredMentores.sort(
+          (a, b) => a.estudiantesAyudados - b.estudiantesAyudados
+        );
         break;
       case 'students-desc':
-        this.filteredMentores.sort((a, b) => b.estudiantesAyudados - a.estudiantesAyudados);
+        this.filteredMentores.sort(
+          (a, b) => b.estudiantesAyudados - a.estudiantesAyudados
+        );
         break;
       case 'price-asc':
         this.filteredMentores.sort((a, b) => a.tarifaPorHora - b.tarifaPorHora);
@@ -136,23 +196,5 @@ export class BusquedaComponent implements OnInit {
   onSortChange(event: any): void {
     this.sortOption = event.target.value;
     this.sortMentores();
-  }
-
-  onSearch(): void {
-    this.filterMentores();
-  }
-
-  getDayPeriod(date: string): string {
-    const day = new Date(date).getDay();
-    switch (day) {
-      case 0: return 'mañana'; // Sunday
-      case 1: return 'mañana'; // Monday
-      case 2: return 'tarde';  // Tuesday
-      case 3: return 'tarde';  // Wednesday
-      case 4: return 'noche';  // Thursday
-      case 5: return 'noche';  // Friday
-      case 6: return 'mañana'; // Saturday
-      default: return '';
-    }
   }
 }
