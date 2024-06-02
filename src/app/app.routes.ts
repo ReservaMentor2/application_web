e4257ebd439cfe4c631037ab9b9907491c8167f1
nat <nathaly.elianev@gmail.com>
2024-06-02 16:13:51 -0500

    add mentor profile header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+//import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: '', redirectTo: 'header', pathMatch: 'full' },
 ];
