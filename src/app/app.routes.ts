2870db049fd23756ea66883be5f6ae44525bcc07
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-06-04 15:07:09 -0500

    Deleted Landing Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 
-import { LandingPageComponent } from './landing-page/landing-page.component';
-
 import { NavBarComponent } from './components/navbar/navbar.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 export const routes: Routes = [
-    { path: 'landing-page', component: LandingPageComponent, title: 'ReservaMentor'},
     { path: 'navbar', component: NavBarComponent},
     { path: 'busqueda', component: BusquedaComponent},
     { path: '**', redirectTo: '/busqueda', pathMatch: 'full'}
