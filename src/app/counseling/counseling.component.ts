6b169f16b47e726e9cd653ddad369f30f76f0a32
nat <nathaly.elianev@gmail.com>
2024-06-10 00:12:37 -0500

    add counseling component

---
+++ src/app/counseling/counseling.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counseling',
+  standalone: true,
+  imports: [],
+  templateUrl: './counseling.component.html',
+  styleUrl: './counseling.component.css'
+})
+export class CounselingComponent {
+
+}
