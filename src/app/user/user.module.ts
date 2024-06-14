4e964a376312303df0a631fc30c1ea596e8ecc72
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-06-14 00:04:27 -0500

    User module

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRouting } from './user-routing.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    UserRouting
+  ]
+})
+
+export class UserModule { }
