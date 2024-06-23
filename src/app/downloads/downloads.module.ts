b79ce45c542f32bac5637ccb4f6b76284e9fc4a3
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-06-23 10:36:33 -0500

    Added download list

--- src/app/downloads/downloads.module.ts
+++ src/app/downloads/downloads.module.ts
@@ -4,17 +4,29 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DownloadPageComponent } from './download-page/download-page.component';
 import { DownloadCardComponent } from './download-card/download-card.component';
 import { DownloadsRoutingModule } from './downloads-routing.module';
+import { CourseMaterialsComponent } from './course-materials/course-materials.component';
+import { MatTableModule } from '@angular/material/table';
+
+interface CourseMaterial {
+  name: string;
+  type: string;
+  size: string;
+}
 
 @NgModule({
   declarations: [
     DownloadPageComponent,
     DownloadCardComponent,
+    CourseMaterialsComponent
   ],
   imports: [
     DownloadsRoutingModule,
     FormsModule,
     CommonModule,
     ReactiveFormsModule,
+    MatTableModule,
+    
   ],
 })
-export class DownloadsModule { }
+export class DownloadsModule {
+}
