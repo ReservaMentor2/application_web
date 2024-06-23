c5fe99f29c98445be5183dbef55727f757f6db90
Mathias <mathiashv2014@gmail.com>
2024-06-23 16:51:34 -0500

    Eliminar sesiones programadas

--- src/app/components/detalles-mentoria/detalles-mentoria.component.ts
+++ src/app/components/detalles-mentoria/detalles-mentoria.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Sesion } from '../../models/sesion';
 import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import * as sesionesData from '../../../assets/sesiones-list.json';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalles-mentoria',
@@ -11,11 +10,35 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './detalles-mentoria.component.html',
   styleUrl: './detalles-mentoria.component.css',
 })
-export class DetallesMentoriaComponent {
+export class DetallesMentoriaComponent implements OnInit {
   sesiones: Sesion[] = [];
 
   ngOnInit() {
-    this.sesiones = (sesionesData as any).default.sesiones;
+    this.cargarSesiones();
+  }
+
+  cargarSesiones() {
+    const sesionesGuardadas = localStorage.getItem('sesiones');
+    if (sesionesGuardadas) {
+      this.sesiones = JSON.parse(sesionesGuardadas);
+    } else {
+      this.sesiones = (sesionesData as any).default.sesiones;
+      this.guardarSesiones();
+    }
     console.dir(this.sesiones);
+  }
+
+  guardarSesiones() {
+    localStorage.setItem('sesiones', JSON.stringify(this.sesiones));
+  }
+
+  confirmarEliminacion(index: number): void {
+    const confirmacion = window.confirm(
+      '¿Estás seguro de que quieres cancelar esta sesión?'
+    );
+    if (confirmacion) {
+      this.sesiones.splice(index, 1);
+      this.guardarSesiones();
+    }
   }
 }
