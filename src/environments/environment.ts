512b768e48a2ffdcb69f57986009fc2e505bb91b
Ibrahim <u20191E650@upc.edu.pe>
2024-06-24 15:45:52 -0500

    build dist

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment={
+export const environment = {
   producction: false,
-  apiUrl: 'http://localhost:3000/api/v1'
-}
+  //apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'https://application-web-api-fake.onrender.com/api/v1',
+};
